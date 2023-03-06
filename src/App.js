bdc0b955b7b9ef31992270817b302d0698301683
Leonardo Augusto <leonardo.augusto@rookau.com>
2023-03-06 20:53:16 -0300

    aula(imc): ex de construcao de imc

--- src/App.js
+++ src/App.js
@@ -1,45 +1,8 @@
-import React, { useState } from "react";
-import TabeladePesos from "./Componentes/TabeladePesos";
-import CalculodePeso from "./Componentes/CalculodePeso";
-import CSS from './index.css'
-
-
-const Resultado = (r) => {
-  return(
-    <div>
-      <p>Resultado: { r?.toFixed(2)} </p>
-    </div>
-  )
-}
-
-
-
+import React from "react";
 
 export default () => {
-  const [peso, setPeso] = useState();
-  const [altura, setAltura] = useState();
-  const [res, setRes] = useState()
-
-
-
-  return (
+  return(
     <>
-      <div className="Container">
-        <h1>Calculadora IMC</h1>
-
-        <TabeladePesos />
-
-
-      <div className="Resultado">
-        <CalculodePeso varP={peso} varSetP={setPeso} varA={altura} varSetA={setAltura} sr={setRes} />
-      </div>
-
-      {Resultado(res)}
-
-      </div>
-
-     
-   
     </>
-  );
-};
+  )
+}
