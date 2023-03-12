0e30cdf07c3c849e8934f98e91c651e9ee9a8702
Leonardo Augusto <leonardo.augusto@rookau.com>
2023-03-12 17:37:37 -0300

    aula(calc): criacao de tela e estilos

--- src/App.js
+++ src/App.js
@@ -1,92 +1,52 @@
-import React, { useState } from "react";
+import React,{useState} from "react";
 
 export default () => {
-  const [valorTela, setValorTela] = useState("");
-  const [resultado, setResultado] = useState(0);
-  const [acumulador, setAcumulador] = useState(0);
-  const [operacao, setOperacao] = useState(false);
+  
 
-  // Estilos
-  const cssTela = {
-    fontSize: 25,
-    color: "#fff",
-    display: "flex",
-    paddingLeft: "20",
-    paddingRight: "20",
-    justifyContent: "center",
-    alingItems: "flex-start",
-    backgroundColor: "#444",
-    flexDirection: "column",
-    window: 260,
-  };
+// States (estados da aplicação)
+const [valorTela, setvalorTela] = useState('')
+const [resultado, setResultado] = useState(0)
+const [acumulador, setAcumulador] = useState(0)
+const [operacao, setOperacao] = useState(false)
 
-  const cssTelaOper = {
-    fontSize: 50,
-    color: "#fff",
-    height: 20,
-  };
 
-  const cssTelaRes = {
-    fontSize: 50,
-    color: "#fff",
-  };
+// Estilos
 
-  const cssBtn = {
-    fontSize: 30,
-    height: 75,
-    width: 75,
-    padding: 20,
-    backgroundColor: "#000",
-    color: "#fff",
-    borderColor: "#000",
-    textAling: "center",
-    outline: "none",
-  };
+const cssTela = {
+  display: 'flex',
+  paddingLeft: 20,
+  paddingReft: 20,
+  justifyContent: 'center',
+  alignItems: 'flex-start',
+  backgroundColor: '#444',
+  flexDirection: 'column',
+  window: 260
+}
 
-  // Funções
+const cssTelaOper = {
+  display: 'flex',
+  paddingLeft: 20,
+  paddingReft: 20,
+  justifyContent: 'center',
+  alignItems: 'flex-start',
+  backgroundColor: '#444',
+  flexDirection: 'column',
+  window: 260
+}
 
-  const addDigitoTela = (digito) => {
-    if (
-      (digito == "+" || digito == "-" || digito == "*" || digito == "/") &&
-      operacao
-    ) {
-      console.log("+-*/");
-      setOperacao(false);
-      setValorTela(resultado + digito);
-      return;
-    } else if (operacao) {
-      setValorTela(digito);
-      setOperacao(false);
-      return;
-    }
-    const valorDigitadoTela = valorTela + digito;
-    setValorTela(valorDigitadoTela);
-  };
 
-  const limparMemomoria = () => {
-    setOperacao(false);
-    setValorTela("");
-    setResultado(0);
-  };
-
-  // Componentes
-  const Tela = (valor, res) => {
-    return (
-      <div style={{ cssTela }}>
-        <span>{valor}</span>
-        <span>{res}</span>
-      </div>
-    );
-  };
-
-  const btn = (label, onclick) => {
-    return(
-      <div>
-        <span style={cssTelaoper}>{valorTela}</span>
-        <span style={cssTelaRes}> {resultado}</span>
-      </div>
-    )
-  }
-
-  return <>{Tela}</>;
-};
+const Tela = (val, res) => {
+  return (
+    <div style={cssTela}>
+      <span>{val}</span>
+      <span>{res}</span>
+    </div>
+  )
+}
+  
+  return(
+    <>
+      {Tela}
+    </>
+  )
+}
