bf9cbf3825139265b7e57f3296c6169efb3e8913
Leonardo Augusto <leonardo.augusto@rookau.com>
2023-04-03 21:41:10 -0300

    aula(teste)

--- src/Componentes/Ex.js
+++ src/Componentes/Ex.js
@@ -1,20 +1,17 @@
 import { useState } from "react";
 import React from "react";
 
-export default ()=>{
+export default () => {
 
     const [teste, setteste] = useState('')
-    return(
+    return (
         <>
             <label>Digite algo:</label>
-            <input type='text' value={teste} onChange={(e)=> setteste(e.target.value)}></input>
+            <input type='text' value={teste} onChange={(e) => setteste(e.target.value)}></input>
             <br></br>
 
             <p>{teste}</p>
 
-
-
-            
         </>
     )
 }
